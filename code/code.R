cb3f19dc03d69886e1fb585c48a5e4e55e1c834a
chelseajia <jiahuibo@seattleu.edu>
2018-11-19 16:49:59 -0800

    data base number of person for each cluster

---
+++ code/code.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(haven)
+library(dplyr)
+library(GGally)
+
+
+sec2a <- read_dta("./raw_data/sec2a.dta")  #data about education
+summary(sec2a)
+count(sec2a)
+
+
+sec2a %>% group_by(sec2a$clust) %>% summarize(count=n())
+sec2a %>% group_by(sec2a$clust,sec2a$nh) %>% summarize(count=n())
+sec2a %>% group_by(sec2a$clust, sec2a$nh,sec2a$pid) %>% summarize(count=n())
+
+
+sec2a %>%
+  group_by(sec2a$clust) %>% 
+  ggplot(aes(x = sec2a$clust)) + geom_bar() + ggtitle("number of person for each cluster") 
+  
