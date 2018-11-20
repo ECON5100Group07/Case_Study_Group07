b4a4c506daf8ba45952405c7913cf2e32e0aeaba
chelseajia <jiahuibo@seattleu.edu>
2018-11-19 17:39:50 -0800

    import relative files for analysis

--- code/code.R
+++ code/code.R
@@ -3,8 +3,15 @@ library(haven)
 library(dplyr)
 library(GGally)
 
+#import files
 sec0a <- read_dta("raw_data/sec0a.dta")  #data about survey info
 sec2a <- read_dta("raw_data/sec2a.dta")  #data about education
+sec8b <- read_dta("raw_data/sec8b.dta") #data about unit measure
+agg2 <- read_dta("data/aggregates/agg2.dta") #data about household agriculture income
+subagg23 <- read_dta("data/aggregates/subagg23.dta") #data about expenditures on crop input per household
+
+
+summary(sec0a)
 summary(sec2a)
 count(sec2a)
 
